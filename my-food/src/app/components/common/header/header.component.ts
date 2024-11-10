b5d728e8c9ffdc59ce531128ceb71f60f0521aa0
Camilo Bonilla <camilobonillab26@gmail.com>
2024-11-09 20:51:54 -0500

    menu

--- my-food/src/app/components/common/header/header.component.ts
+++ my-food/src/app/components/common/header/header.component.ts
@@ -3,12 +3,13 @@ import { NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
 import { OrderComponent } from '../../order/order/order.component';
 import { InventoryComponent } from "../../inventory/inventory.component";
 import { WasteComponent  } from '../../waste/waste.component';
+import { MenuComponent } from '../../menu/menu.component';
 
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [NgbNavModule, OrderComponent, InventoryComponent, WasteComponent],
+  imports: [NgbNavModule, OrderComponent, InventoryComponent, WasteComponent, MenuComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
